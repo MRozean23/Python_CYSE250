6f54c47d15f79282581a99b6fd5c463fed2bcf35
MRozean23 <64090203+MRozean23@users.noreply.github.com>
2023-11-30 11:41:18 -0800

    Add files via upload

---
+++ projectMain.py
@@ -0,0 +1,44 @@
+def main(): 
+    "Storing User Input as variables"
+    webName = input("Enter name of Website: ")
+    passLength = int(input("Enter number of characters for your password: "))
+    pswd = []
+    secret = pswdGenerator(passLength, pswd)
+
+    "Outputs my program's functions"
+    print(errorMessage(passLength))
+    pswdGenerator(passLength, pswd)
+    passManager(webName, secret)
+    input("Press Enter to exit")
+    
+def errorMessage(passLength):
+    #Generate an error message for the user if passLength < 10 characters
+    if passLength < 10:
+        return "Error: For better security passwords should be at least 10 characters in length."
+    else:
+        return "Your password will be generated shortly..."
+
+def pswdGenerator(passLength, pswd):
+    #Loops through passLength generating a randomn password
+    import random
+    secret = ""
+    for num in range(1, passLength + 1):
+        num = chr(random.randint(ord('!'), ord('~')))
+        pswd.append(num)
+    for char in pswd:
+        secret += char
+    return secret
+
+def passManager(webName, secret):
+    myDict = {}
+    myDict.update({secret : webName})
+
+    for v in myDict.values():
+        webSite = v
+    for k in myDict.keys():
+        passWord = k
+
+    return print("Thank you for using PassGen :)\nYour generated password for",webSite,"is: ",passWord)
+
+if __name__ == "__main__":
+    main()
