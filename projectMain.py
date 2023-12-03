0640135ce8ecf0b816a3091f7c8970169dac0b0b
MRozean23 <64090203+MRozean23@users.noreply.github.com>
2023-12-03 12:21:52 -0800

    Added code for creating a server

--- projectMain.py
+++ projectMain.py
@@ -1,25 +1,25 @@
 def main(): 
-    "Storing User Input as variables"
+    # Storing User Input as variables
     webName = input("Enter name of Website: ")
     passLength = int(input("Enter number of characters for your password: "))
     pswd = []
     secret = pswdGenerator(passLength, pswd)
 
-    "Outputs my program's functions"
+    # Outputs my program's functions
     print(errorMessage(passLength))
     pswdGenerator(passLength, pswd)
     passManager(webName, secret)
     input("Press Enter to exit")
     
 def errorMessage(passLength):
-    #Generate an error message for the user if passLength < 10 characters
+    # Generate an error message for the user if passLength < 10 characters
     if passLength < 10:
         return "Error: For better security passwords should be at least 10 characters in length."
     else:
         return "Your password will be generated shortly..."
 
 def pswdGenerator(passLength, pswd):
-    #Loops through passLength generating a randomn password
+    # Loops through passLength generating a randomn password
     import random
     secret = ""
     for num in range(1, passLength + 1):
@@ -30,15 +30,16 @@ def pswdGenerator(passLength, pswd):
     return secret
 
 def passManager(webName, secret):
+    # Creates a dictionary to store the weName and secret
     myDict = {}
     myDict.update({secret : webName})
 
     for v in myDict.values():
-        webSite = v
+        webSite = str(v)
     for k in myDict.keys():
-        passWord = k
-
-    return print("Thank you for using PassGen :)\nYour generated password for",webSite,"is: ",passWord)
+        passWord = str(k)
+    complete = webSite +" - "+ passWord
+    return print(complete)
 
 if __name__ == "__main__":
     main()
